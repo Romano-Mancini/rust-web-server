f106b118f96b55b0e87e2408c319127b52670262
Romano-Mancini <romanomancini03@gmail.com>
2026-09-20 19:57:20 +0200

    feat: added single-thread functionalities

---
+++ web-server/src/main.rs
@@ -0,0 +1,39 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Error, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader: BufReader<&TcpStream> = BufReader::new(&stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result: Result<String, Error>| result.unwrap())
+        .take_while(|line: &String| !line.is_empty())
+        .collect();
+
+    let request_path = http_request[0].split_whitespace().skip(1).next().unwrap();
+    let (status_line, file_name) = match request_path {
+        "/" => ("HTTP/1.1 200 OK", "index.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "not_found.html"),
+    };
+    let response = fs::read_to_string(file_name).unwrap();
+    let length = response.len();
+
+    stream
+        .write(format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{response}").as_bytes())
+        .unwrap();
+
+    println!("{http_request:#?}");
+}
+
+fn main() {
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
